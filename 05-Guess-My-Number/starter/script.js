707e2a679f5c6b35b005d9dc4fe6931a217129d2
Jami Karimkulov <jami.karimkul@gmail.com>
2024-05-23 16:44:51 -0400

    Add 05-Guess-My-Number

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'start guessing ...';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 0;
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
